74d003621fb3fcbdfa5a35e50182c22458cdf859
MarcAT92 <94722932+MarcAT92@users.noreply.github.com>
2024-04-03 20:33:30 -0400

    update
    changed zip file for all certs to merged pdf and moved popup cookies div to footer

--- js/scripts.js
+++ js/scripts.js
@@ -183,10 +183,10 @@
         document.getElementById('cookieConsent').style.display = 'none';
     }
 
-    // Check if the user has already accepted cookies
+   /* // Check if the user has already accepted cookies
     window.onload = function() {
         if (document.cookie.split(';').some((item) => item.trim().startsWith('acceptedCookies='))) {
             // If 'acceptedCookies' cookie is found, hide the cookie consent
             document.getElementById('cookieConsent').style.display = 'none';
         }
-    } 
+    }  */
