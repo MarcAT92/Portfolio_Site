921fd305c6c0a44aff98033ded7d0a932bb9a34d
MarcAT92 <94722932+MarcAT92@users.noreply.github.com>
2024-09-27 16:47:11 -0400

    updated

--- js/config.js
+++
@@ -1,8 +0,0 @@
-const emailConfig = {
-    senderEmail: "marc.a.thomas25@gmail.com",
-    recipientEmail: "marc.a.thomas25@gmail.com",
-    secureToken: "93dc7028-ced8-4345-88bb-64271c09a6e4",
-  };
-  
-  export default emailConfig;
-  
